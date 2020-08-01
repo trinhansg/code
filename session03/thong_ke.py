67401a8fea067167a743656f3f0a36acfa7bae7c
Tri Nhan <trinhansg2020@gmail.com>
2020-08-01 13:30:45 +0700

    updated

---
+++ session03/thong_ke.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+x = np.array([1,2,3,4,5])
+
+print(np.sum(x))
+print(np.min(x))
+print(np.max(x))
+print(np.average(x))
+print(np.mean(x))
+print(np.median(x))
+print(np.size(x))
+print(np.var(x))
+print(np.std(x))
