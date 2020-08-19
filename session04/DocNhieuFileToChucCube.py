11555319918245797e26ed65982803d0a5f1de24
Tri Nhan <trinhansg2020@gmail.com>
2020-08-19 13:34:47 +0700

    updated

--- session04/DocNhieuFileToChucCube.py
+++ session04/DocNhieuFileToChucCube.py
@@ -9,8 +9,9 @@ def readfilecsv(filename):
         DongChiTiet=[]
         for row in readCSV:
             print(row)
-            if row==[]:
-                break
+            # if row==[]:
+            #     pass
+            #     # break
             if i==1:
                 DongTieuDe=row
                 i=i+1
@@ -22,8 +23,10 @@ def readfilecsv(filename):
     return DongTieuDe,DongChiTiet
 
 
+
 DongTieuDe1,DongChiTiet1=readfilecsv('data/kqdq_ads.csv')
 DongTieuDe2,DongChiTiet2=readfilecsv('data/kqdq_aav.csv')
+DongTieuDe3,DongChiTiet3=readfilecsv('data/kqdq_aam.csv')
 print("**** Dong tieu de 1-data/kqdq_ads.csv ")
 print(DongTieuDe1)
 print("Dong tieu de 2data/kqdq_aav.csv")
@@ -32,12 +35,43 @@ print(DongTieuDe2)
 Cube=[]
 Cube.append(DongChiTiet1)
 Cube.append(DongChiTiet2)
+Cube.append(DongChiTiet3)
 print("Phan tu cua mang 3 chieu")
 print("Cong ty Dong tieu de 1-data/kqdq_ads.csv")
 print(Cube[0][0])
-print(Cube[0][0][0])
+print(Cube[0][0][0])#Cong ty, dong, cot
+print(Cube[0][0][1])
+print(Cube[0][0][2])
 
 print("Cong ty Dong tieu de 2-data/kqdq_aav.csv")
 print(Cube[1][0])
 print(Cube[1][0][0])
 print(Cube[1][0][1])
+print(Cube[1][0][2])
+
+print("Cong ty Dong tieu de 3-data/kqdq_aam.csv")
+print(Cube[2][0])
+print(Cube[2][0][0])
+print(Cube[2][0][1])
+print(Cube[2][0][2])
+# ROA=loinhuan/Tongtaisan
+print('Loi nhuan: ',Cube[2][48])
+print('Tong tai san: ',Cube[2][16])
+print(f"ROA: {float(Cube[2][48][1])/float(Cube[2][16][1]):,.6f}")
+roa = float(Cube[2][48][1])/float(Cube[2][16][1])
+# ROS=loinhuan/Doanhthuthuan
+print('Doanh thu thuan: ',Cube[2][33])
+print(f"ROS: {float(Cube[2][48][1])/float(Cube[2][33][1]):,.6f}")
+ros = float(Cube[2][48][1])/float(Cube[2][33][1])
+print(f"AAM,{roa},{ros}")
+
+companies = ['ads','aav','aam']
+
+f = open("data/data.csv", "w", encoding='utf8')
+for i in range(len(companies)):
+    roa = float(Cube[i][48][1]) / float(Cube[i][16][1])
+    ros = float(Cube[i][48][1]) / float(Cube[i][33][1])
+    print(f"{companies[i]},{roa},{ros}\n")
+    f.write(f"{companies[i]},{roa},{ros}\n")
+f.close()
+
